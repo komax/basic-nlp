166236ad81d704e7a27b539a245cdee9a8f980f2
Maximilian Konzack <maximilian.konzack@idiv.de>
2018-07-24 17:01:02 +0200

    Generate visualizations from spacy

--- calling_spacy.py
+++ calling_spacy.py
@@ -1,4 +1,20 @@
+from pathlib import Path
+
 import spacy
+from spacy import displacy
+
+
+def visualize_doc(doc, output_dir, input_name, options={'compact':True}):
+    figures = [('dep', f'{input_name}_dependency_parse.html'),
+               ('ent', f'{input_name}_entity_recognition.html')]
+    for style, file_name in figures:
+        vis_html = displacy.render(doc, style=style, page=True, options=options)
+        output_path = Path(f'{output_dir}/{file_name}')
+        output_path.open('w', encoding='utf-8').write(vis_html)
+
+
+def file_name(input_text):
+    return Path(input_text).stem
 
 
 def nlp_spacy(input_text, output_dir):
@@ -7,5 +23,9 @@ def nlp_spacy(input_text, output_dir):
         text = input_file.read()
     nlp = spacy.load('en_core_web_sm')
 
+    # Run spacy's nlp.
     doc = nlp(text)
-    doc.to_disk(f"{output_dir}/spacy_document.bin")
+
+    text_file_name = file_name(input_text.name)
+    # Generate visualizations from the document.
+    visualize_doc(doc, output_dir, text_file_name)
