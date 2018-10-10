6b3db0014468c0ce25702d69590c15ab4fc717eb
Maximilian Konzack <maximilian.konzack@idiv.de>
2018-10-10 15:19:06 +0200

    Apply script in parallel for a directory

--- remove-pdf-annotations.sh
+++ remove-pdf-annotations.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-pdftk $1 output uncompressed.pdf uncompress
-LANG=C LC_CTYPE=C sed -n '/^\/Annots/!p' uncompressed.pdf > stripped.pdf
-pdftk stripped.pdf output $1 compress
-rm uncompressed.pdf stripped.pdf
+function remove_annotation {
+    echo "Removing annotations from $1"
+    pdftk $1 output $1.uncompressed uncompress
+    LANG=C LC_CTYPE=C sed -n '/^\/Annots/!p' $1.uncompressed > $1.stripped
+    pdftk $1.stripped output $1 compress
+    rm $1.uncompressed $1.stripped
+}
+
+input=$1
+
+if [[ -d $input ]]; then
+    # from https://stackoverflow.com/questions/6481005/how-to-obtain-the-number-of-cpus-cores-in-linux-from-the-command-line
+    cpu_count=`getconf _NPROCESSORS_ONLN`
+    ls $input/*.pdf | parallel -j ${cpu_count} "./remove-pdf-annotations.sh {}"
+#    for pdf in $input/*.pdf
+#    do
+#        remove_annotation $pdf
+#    done
+elif [[ -f $input && ${input: -4} == ".pdf" ]]; then
+    remove_annotation $input
+else
+    echo "Please provide a directory containing pdfs or a pdf"
+    exit 1
+fi
+
+
 
